194702e24d0b311abc28caef2a83a924df63f666
Nate Landon <druidprogramming@gmail.com>
2018-03-29 12:37:22 -0600

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ElectronNET.API;
+using ElectronNET.API.Entities;
+using System.IO;
+
+
+
+namespace MyElectronMusicPlayer.Controllers
+{
+    public class HomeController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            string musicPath = await Electron.App.GetPathAsync(PathName.music);
+            string[] mp3Files = Directory.GetFiles(musicPath, "*.mp3, SearchOption.TopDirectoryOnly");
+            string[] mp4Files = Directory.GetFiles(musicPath, "*.mp4, SearchOption.TopDirectoryOnly");
+
+            List<string> musicFiles = new List<string>();
+            musicFiles.AddRange(mp3Files);
+            musicFiles.AddRange(mp4Files);
+            return View(musicFiles);
+        }
+    }
+}
